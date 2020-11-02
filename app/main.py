30048264ba05ca7008a5f54e02a7deae5ba8ebe2
Jon Herbst <herbtaculargames@gmail.com>
2020-11-02 12:16:08 +0000

    Adding basic /dashboard API endpoint with placeholder responses for test passing

--- app/main.py
+++ app/main.py
@@ -119,6 +119,26 @@ def logout():
         'message': message
     }), 200
 
+@app.route('/dashboard', methods=['GET'])
+@jwt_required
+def dashboard():
+    claims = get_jwt_claims()
+    username = get_jwt_identity()
+
+    if not username:
+        error = 'Invalid username identity.'
+    else:
+        return jsonify({
+            'live_view': ['1'],
+            'stats': ['2'],
+            'last_match': ['3'],
+            'previous_matches': ['4']
+        }), 200
+    
+    return jsonify({
+        'message': error
+    }), 400
+
 @app.route('/profile/<user_id>', methods=['GET'])
 @jwt_required
 def profile(user_id):
