babd3a82abffb1c6f50cccdf09f1586720a7f219
Jon Herbst <herbtaculargames@gmail.com>
2020-11-11 12:00:43 +0000

    Begin setup of first match viewing endpoint tests

--- tests/test_live.py
+++ tests/test_live.py
@@ -21,15 +21,45 @@ class TestLiveMatchViewing:
         app.config['USERNAME'] = 'admin'
         app.config['PASSWORD'] = 'password'
         app.config['ACTIVATION_KEY'] = '08fe47e8814b410cbaf742463e8c9252'
+        app.config['ACTIVATION_KEY_2'] = '97a56754b27e4cbea94e6c7ca9884b2b'
 
         db.create_all()
 
-        new_key = Activation(app.config['ACTIVATION_KEY'])
-        """Use a fixed activation key string for testing purposes."""
-        db.session.add(new_key)
+        key_first = Activation(app.config['ACTIVATION_KEY'])
+        key_second = Activation(app.config['ACTIVATION_KEY_2'])
+        """Use fixed activation key strings for testing purposes."""
+        db.session.add(key_first)
+        db.session.add(key_second)
         db.session.commit()
 
         with app.test_client(self) as client:
             yield client
 
         db.drop_all()
+
+    def testViewSingleTurn(self, client):
+        rv = register(client, app.config['EMAIL'], app.config['USERNAME'], app.config['PASSWORD'], app.config['ACTIVATION_KEY'])
+        rv = login(client, app.config['EMAIL'], app.config['PASSWORD'])
+
+        access_token = json.loads(rv.data)['access_token']
+        rv = newMatch(
+            client,
+            getMatchData(),
+            access_token
+        )
+
+        uuid = json.loads(rv.data)['uuid']
+        response = startMatch(
+            client,
+            uuid,
+            access_token
+        )
+
+        rv = viewTurn(
+            client,
+            uuid,
+            1
+            access_token
+        )
+
+        assert 200 == rv.status_code
