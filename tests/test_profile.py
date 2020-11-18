f154767905116277c5ba2e4019076e2a97cf7c33
Jon Herbst <herbtaculargames@gmail.com>
2020-11-18 17:49:47 +0000

    Adding new match history API endpoint testing, arranging imports for PEP8 guidelines and encountering new bug with new match creation only looking for singular occurences during setup

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,11 +1,14 @@
-import datetime, json, pytest
+import datetime
+import json
+import pytest
 
 from pathlib import Path
 
-from app.models.activation import Activation
-from app.main import create_app
 from app.database import db
-from tests.utils import login, logout, profile, register
+from app.main import create_app
+from app.models.activation import Activation
+from tests.helpers import getMatchData, getSimulationTurnData
+from tests.utils import endMatch, login, logout, newMatch, nextTurn, profile, register, startMatch
 
 TEST_DB = 'test.db'
 
@@ -76,3 +79,69 @@ class TestProtectedProfile:
 
         assert 400 == response.status_code
         assert 'Cannot retrieve data from another user.' in response.json['message']
+
+    def testMatchHistory(self, client):
+        rv = register(client, self.app.config['EMAIL'], self.app.config['USERNAME'], self.app.config['PASSWORD'], self.app.config['ACTIVATION_KEY'])
+        rv = login(client, self.app.config['EMAIL'], self.app.config['PASSWORD'])
+
+        access_token = json.loads(rv.data)['access_token']
+        uuids = []
+
+        for m in range(5):
+            rv = newMatch(
+                client,
+                getMatchData(),
+                access_token
+            )
+
+            uuids.append(json.loads(rv.data)['uuid'])
+            response = startMatch(
+                client,
+                uuids[m],
+                access_token
+            )
+
+            for n in range(10):
+                rv = nextTurn(
+                    client,
+                    uuids[m],
+                    getSimulationTurnData(n),
+                    access_token
+                )
+
+                assert 200 == rv.status_code
+
+            response = endMatch(
+                client,
+                uuids[m],
+                access_token
+            )
+
+        response = viewHistory(
+            client,
+            1,
+            access_token
+        )
+
+        assert 200 == response.status_code
+
+        assert response.json['match_history']
+        assert 5 == len(response.json['match_history'])
+
+        assert uuids[0] in response.json['match_history'][0]['id']
+        assert uuids[1] in response.json['match_history'][1]['id']
+        assert uuids[2] in response.json['match_history'][2]['id']
+        assert uuids[3] in response.json['match_history'][3]['id']
+        assert uuids[4] in response.json['match_history'][4]['id']
+
+        assert ('Match 1 - ' + uuids[0]) in response.json['match_history'][0]['name']
+        assert ('Match 1 - ' + uuids[1]) in response.json['match_history'][1]['name']
+        assert ('Match 1 - ' + uuids[2]) in response.json['match_history'][2]['name']
+        assert ('Match 1 - ' + uuids[3]) in response.json['match_history'][3]['name']
+        assert ('Match 1 - ' + uuids[4]) in response.json['match_history'][4]['name']
+
+        assert int(datetime.datetime.utcnow().timestamp()) < response.json['match_history'][0]['ended_at']
+        assert int(datetime.datetime.utcnow().timestamp()) < response.json['match_history'][1]['ended_at']
+        assert int(datetime.datetime.utcnow().timestamp()) < response.json['match_history'][2]['ended_at']
+        assert int(datetime.datetime.utcnow().timestamp()) < response.json['match_history'][3]['ended_at']
+        assert int(datetime.datetime.utcnow().timestamp()) < response.json['match_history'][4]['ended_at']
