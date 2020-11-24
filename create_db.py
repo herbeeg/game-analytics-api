17a7d01461bc360014a62e206c71fc607c164633
Jon Herbst <herbtaculargames@gmail.com>
2020-11-24 11:18:31 +0000

    Add app context when trying to scaffold database tables when deploying new changes to Heroku so new columns and modifications can be applied in a fresh context

--- create_db.py
+++ create_db.py
@@ -1,7 +1,9 @@
 from app.database import db
+from app.main import create_app
 
-db.create_all()
-"""Create the database and requisite tables."""
+with create_app().app_context():
+    db.create_all()
+    """Create the database and requisite tables."""
 
-db.session.commit()
-"""Make said changes."""
+    db.session.commit()
+    """Make said changes."""
